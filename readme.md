c17e15cf6930960999618c9fa697f1de14881306
SpliiT <spliitdev@gmail.com>
2024-01-22 23:03:07 +0100

    first commit

---
+++ readme.md
@@ -0,0 +1,42 @@
+# Tic Tac Doh
+
+![Accueil de Tic Tac Doh](tic-tac-doh.jpg)
+
+## Description
+
+Tic Tac Doh est une version du célèbre jeu du "Morpion" sous le thème des Simpsons. Ce jeu a été réalisé dans le cadre d'un projet pour La Fabrique Numérique.
+
+
+## Fonctionnalités
+
+- Gameplay classique du Tic Tac Toe.
+- Design responsive pour une expérience fluide sur différents appareils.
+
+## Technologies Utilisées
+
+- HTML5
+- CSS3
+- JavaScript
+
+## Installation
+
+1. Clonez le dépôt : `git clone https://github.com/votre-nom-utilisateur/tic-tac-doh.git`
+2. Ouvrez le fichier `index.html` dans votre navigateur web préféré.
+
+## Utilisation
+
+- Lancez le jeu dans votre navigateur.
+- Jouez au Tic Tac Toe avec un ami et amusez-vous !
+
+## Gameplay
+
+![Capture d'écran du gameplay](gameplay.jpg)
+
+
+## Victoires
+
+![Capture d'écran de la victoire team saxo](winsaxo.jpg)
+![Capture d'écran de la victoire team skate](winskate.jpg)
+
+## Match nul
+![Capture d'écran du match nul](nulscreen.jpg)
---
+++ script.js
@@ -0,0 +1,138 @@
+// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠁⢸⣿⣿⣿⡀⠙⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋⡀⡀⢸⣿⣿⣿⡀⡀⡀⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠋⡀⡀⡀⡀⢸⣿⣿⣿⡀⡀⡀⡀⠈⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⡀⡀⡀⡀⡀⡀⢸⣿⣿⣿⡀⡀⡀⡀⡀⡀⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠁⡀⡀⡀⡀⡀⡀⡀⢸⣿⣿⣿⡀⡀⡀⡀⡀⡀⡀⡀⠙⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠁⡀⡀⡀⡀⢀⡆⡀⡀⡀⢸⣿⣿⣿⡀⡀⡀⡀⣆⡀⡀⡀⡀⡀⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠋⡀⡀⡀⡀⡀⣴⣿⡇⡀⡀⡀⢸⣿⠈⠛⡀⡀⡀⡀⣿⣿⣄⡀⡀⡀⡀⠈⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+// ⣿⣿⣿⣿⣿⣿⣿⣿⠟⡀⡀⡀⡀⡀⣠⣿⣿⣿⡇⡀⡀⡀⢸⣿⡀⡀⡀⡀⡀⡀⣿⣿⣿⣷⡀⡀⡀⡀⡀⠙⢿⣿⣿⣿⣿⣿⣿⣿
+// ⣿⣿⣿⣿⣿⣿⠟⠁⡀⡀⡀⡀⣠⣾⣿⣿⣿⣿⡇⡀⡀⡀⢸⣿⡀⡀⡀⡀⡀⡀⣿⣿⣿⣿⣿⣦⡀⡀⡀⡀⡀⠙⣿⣿⣿⣿⣿⣿
+// ⣿⣿⣿⣿⡿⠁⡀⡀⡀⡀⢀⣾⣿⣿⣿⣿⣿⣿⡇⡀⡀⡀⢸⣿⣷⡀⡀⡀⡀⡀⣿⣿⣿⣿⣿⣿⣿⣤⡀⡀⡀⡀⡀⠻⣿⣿⣿⣿
+// ⣿⣿⣿⠋⡀⡀⡀⡀⡀⣴⣿⣿⣿⣿⣿⣿⣿⣿⡇⡀⡀⡀⡀⠙⣿⣿⣦⡀⡀⡀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣄⡀⡀⡀⡀⠈⢿⣿⣿
+// ⣿⠟⡀⡀⡀⡀⡀⣠⣿⣿⣿⣿⣿⡿⣿⣿⣿⣿⣿⣄⡀⡀⡀⡀⡀⠻⣿⣿⣄⡀⣿⣿⣿⣿⠻⣿⣿⣿⣿⣿⣷⡀⡀⡀⡀⡀⠈⢿
+// ⣿⣄⡀⡀⡀⡀⡀⠻⣿⣿⣿⣿⠋⡀⣿⣿⣿⣿⡿⣿⣿⣄⡀⡀⡀⡀⠈⠻⣿⣷⣿⣿⣿⣿⡀⠈⢿⣿⣿⣿⡿⠉⡀⡀⡀⡀⢀⣾
+// ⣿⣿⣷⡀⡀⡀⡀⡀⠈⢿⠟⡀⡀⡀⣿⣿⣿⣿⡇⠈⢿⣿⣷⡀⡀⡀⡀⡀⠈⢿⣿⣿⣿⣿⡀⡀⡀⠙⣿⠋⡀⡀⡀⡀⡀⣴⣿⣿
+// ⣿⣿⣿⣿⣷⡀⡀⡀⡀⡀⡀⡀⡀⡀⣿⣿⣿⣿⡇⡀⡀⠙⣿⣿⣦⡀⡀⡀⡀⡀⣿⣿⣿⣿⡀⡀⡀⡀⡀⡀⡀⡀⡀⣴⣿⣿⣿⣿
+// ⣿⣿⣿⣿⣿⣿⣦⡀⡀⡀⡀⡀⢀⣾⣿⣿⣿⣿⡇⡀⡀⡀⡀⠻⣿⣿⡀⡀⡀⡀⣿⣿⣿⣿⣿⣄⡀⡀⡀⡀⡀⣠⣿⣿⣿⣿⣿⣿
+// ⣿⣿⣿⣿⣿⣿⣿⣿⣄⡀⢀⣴⣿⣿⣿⣿⣿⣿⡇⡀⡀⡀⡀⡀⣿⣿⡀⡀⡀⡀⣿⣿⣿⣿⣿⣿⣿⣄⡀⢀⣾⣿⣿⣿⣿⣿⣿⣿
+// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣿⣿⣿⣿⣿⣿⣿⣿⡇⡀⡀⡀⡀⡀⣿⣿⡀⡀⡀⡀⣿⣿⣿⣿⣿⣿⣿⣿⣷⣿⣿⣿⣿⣿⣿⣿⣿⣿
+// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⡀⡀⡀⢠⡀⣿⣿⡀⡀⡀⡀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⡀⡀⡀⢸⣿⣿⣿⡀⡀⡀⡀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⡀⡀⡀⢸⣿⣿⣿⡀⡀⡀⡀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⡀⡀⡀⢸⣿⣿⣿⡀⡀⡀⡀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀⡀⡀⢸⣿⣿⣿⡀⡀⡀⣤⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⡀⢸⣿⣿⣿⡀⣠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+
+// Sélection de l'élément du plateau
+let plateau = document.getElementById('plateau');
+
+// Sélection de tous les éléments avec la classe 'case'
+let cases = document.getElementsByClassName('case');
+
+// Initialisation de l'état du jeu (vides au début)
+let etatJeu = ['', '', '', '', '', '', '', '', ''];
+
+// Initialisation du joueur courant ('saxo' commence)
+let joueurCourant = 'saxo';
+
+// Création d'un nouvel élément pour afficher les images de résultat
+let resultatContainer = document.createElement('div');
+resultatContainer.classList.add('resultat-container');
+document.body.appendChild(resultatContainer);
+
+// Boucle qui parcourt toutes les cases
+for (let i = 0; i < cases.length; i++) {
+  // Ajout d'un écouteur d'événements de clic à chaque case
+  cases[i].addEventListener('click', function () {
+    // Vérification de si la case est vide et si le jeu n'est pas encore gagné
+    if (etatJeu[i] === '' && !verifierGagnant()) {
+      // Mise à jour de l'état du jeu avec le symbole du joueur courant
+      etatJeu[i] = joueurCourant;
+
+      // Création d'une image en fonction du joueur courant
+      let img = document.createElement('img');
+      img.src = joueurCourant === 'saxo' ? 'saxo.png' : 'skate.png';
+      img.alt = joueurCourant;
+
+      // Ajout d'une classe pour le style (à l'exception de 'imagecentre' pour saxo.png et skate.png)
+      if (joueurCourant !== 'saxo' && joueurCourant !== 'skate') {
+        img.classList.add('imagecentre');
+      }
+
+      // Suppression du contenu de la case et ajout de l'image
+      cases[i].innerHTML = '';
+      cases[i].appendChild(img);
+
+      // Vérification de s'il y a un gagnant après chaque coup
+      if (verifierGagnant()) {
+        // Affichage de l'image du joueur gagnant dans le nouvel élément
+        afficherImageGagnante(joueurCourant);
+        // Masquer le plateau et le nouvel élément
+        masquerPlateauEtResultat();
+      } else if (!etatJeu.includes('')) {
+        // Si toutes les cases sont remplies et aucun gagnant, affichage de l'image du match nul dans le nouvel élément
+        afficherImageMatchNul();
+        // Masquer le plateau et le nouvel élément
+        masquerPlateauEtResultat();
+      } else {
+        // Passage au joueur suivant
+        joueurCourant = joueurCourant === 'saxo' ? 'skate' : 'saxo';
+      }
+    }
+  });
+}
+
+// Fonction pour vérifier s'il y a un gagnant
+function verifierGagnant() {
+  // Définition des lignes, des colonnes et des diagonales gagnantes
+  const lignes = [[0, 1, 2], [3, 4, 5], [6, 7, 8]];
+  const colonnes = [[0, 3, 6], [1, 4, 7], [2, 5, 8]];
+  const diagonales = [[0, 4, 8], [2, 4, 6]];
+
+  // Fonction interne pour vérifier si une ligne est gagnante
+  function ligneGagnante(ligne) {
+    const symbole = etatJeu[ligne[0]];
+    return symbole !== '' && ligne.every((indiceCase) => etatJeu[indiceCase] === symbole);
+  }
+
+  // Retourne vrai si une ligne, colonne ou diagonale est gagnante
+  return lignes.some(ligneGagnante) || colonnes.some(ligneGagnante) || diagonales.some(ligneGagnante);
+}
+
+// Fonction pour afficher l'image du joueur gagnant
+function afficherImageGagnante(joueur) {
+  // Création d'une image en fonction du joueur gagnant
+  let img = document.createElement('img');
+  img.src = joueur === 'saxo' ? 'saxowin.jpg' : 'skatewin.jpg';
+  img.alt = joueur + ' a gagné !';
+
+  // Ajout d'une classe pour le style
+  img.classList.add('imagecentre');
+
+  // Suppression du contenu du nouvel élément et ajout de l'image
+  resultatContainer.innerHTML = '';
+  resultatContainer.appendChild(img);
+}
+
+// Fonction pour afficher l'image du match nul
+function afficherImageMatchNul() {
+  // Création d'une image pour le match nul
+  let img = document.createElement('img');
+  img.src = 'matchnul.jpg';
+  img.alt = 'Match nul !';
+
+  // Ajout d'une classe pour le style
+  img.classList.add('imagecentre');
+
+  // Suppression du contenu du nouvel élément et ajout de l'image
+  resultatContainer.innerHTML = '';
+  resultatContainer.appendChild(img);
+}
+
+// Fonction pour masquer le plateau et le nouvel élément
+function masquerPlateauEtResultat() {
+  plateau.style.display = 'none';
+  resultatContainer.style.display = 'block';
+}
