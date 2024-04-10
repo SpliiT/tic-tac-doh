ca960b4a7e7e166abd45c2ae6c5b4b163bb8a520
SpliiT <spliitdev@gmail.com>
2024-04-10 11:33:07 +0200

    ultimate commit

--- script.js
+++ script.js
@@ -26,69 +26,69 @@
 // ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
 
 // Récupération de l'élément plateau
-let plateau = document.getElementById('plateau');
+let plateau = document.getElementById("plateau");
 
 // Récupération des éléments de classe 'case'
-let cases = document.getElementsByClassName('case');
+let cases = document.getElementsByClassName("case");
 
 // Initialisation de l'état du jeu
-let etatJeu = ['', '', '', '', '', '', '', '', ''];
+let etatJeu = ["", "", "", "", "", "", "", "", ""];
 
 // Initialisation du joueur courant
-let joueurCourant = 'saxo';
+let joueurCourant = "saxo";
 
 // Création d'un conteneur pour afficher les résultats
-let resultatContainer = document.createElement('div');
-resultatContainer.classList.add('resultat-container');
+let resultatContainer = document.createElement("div");
+resultatContainer.classList.add("resultat-container");
 document.body.appendChild(resultatContainer);
 
 // Initialisation du mode de jeu
-let modeJeu = 'joueur';
+let modeJeu = "ordinateurPro";
 
 // Fonction pour changer le mode de jeu
 function changerModeJeu(mode) {
   modeJeu = mode;
   reinitialiserJeu();
-  if (modeJeu === 'ordinateurPro' && joueurCourant === 'skate') {
+  if (modeJeu === "ordinateurPro" && joueurCourant === "skate") {
     jouerTourOrdinateurPro();
   }
 }
 
 // Fonction de réinitialisation du jeu
 function reinitialiserJeu() {
-  etatJeu = ['', '', '', '', '', '', '', '', ''];
-  joueurCourant = 'saxo';
-  plateau.style.display = 'grid';
-  resultatContainer.style.display = 'none';
+  etatJeu = ["", "", "", "", "", "", "", "", ""];
+  joueurCourant = "saxo";
+  plateau.style.display = "grid";
+  resultatContainer.style.display = "none";
 
   // Réinitialisation du contenu des cases
   for (let i = 0; i < cases.length; i++) {
-    cases[i].innerHTML = '';
+    cases[i].innerHTML = "";
   }
 
   // Si le mode de jeu est 'ordinateur' et le joueur courant est 'skate', jouer le tour de l'ordinateur
-  if (modeJeu === 'ordinateur' && joueurCourant === 'skate') {
+  if (modeJeu === "ordinateur" && joueurCourant === "skate") {
     jouerTourOrdinateur();
   }
   // Si le mode de jeu est 'ordinateurPro' et le joueur courant est 'skate', jouer le tour de l'ordinateur pro
-  else if (modeJeu === 'ordinateurPro' && joueurCourant === 'skate') {
+  else if (modeJeu === "ordinateurPro" && joueurCourant === "skate") {
     jouerTourOrdinateurPro();
   }
 }
 
 // Ajout d'un écouteur d'événement sur chaque case
 for (let i = 0; i < cases.length; i++) {
-  cases[i].addEventListener('click', function () {
+  cases[i].addEventListener("click", function () {
     // Vérification si la case est vide et s'il n'y a pas de gagnant
-    if (etatJeu[i] === '' && !verifierGagnant()) {
+    if (etatJeu[i] === "" && !verifierGagnant()) {
       etatJeu[i] = joueurCourant;
-      let img = document.createElement('img');
-      img.src = joueurCourant === 'saxo' ? 'saxo.png' : 'skate.png';
+      let img = document.createElement("img");
+      img.src = joueurCourant === "saxo" ? "saxo.png" : "skate.png";
       img.alt = joueurCourant;
-      if (joueurCourant !== 'saxo' && joueurCourant !== 'skate') {
-        img.classList.add('imagecentre');
+      if (joueurCourant !== "saxo" && joueurCourant !== "skate") {
+        img.classList.add("imagecentre");
       }
-      cases[i].innerHTML = '';
+      cases[i].innerHTML = "";
       cases[i].appendChild(img);
 
       // Si un joueur a gagné, afficher l'image de la victoire et masquer le plateau et les résultats
@@ -97,16 +97,16 @@ for (let i = 0; i < cases.length; i++) {
         masquerPlateauEtResultat();
       }
       // Sinon, s'il n'y a plus de cases vides, afficher l'image du match nul et masquer le plateau et les résultats
-      else if (!etatJeu.includes('')) {
+      else if (!etatJeu.includes("")) {
         afficherImageMatchNul();
         masquerPlateauEtResultat();
       }
       // Sinon, changer le joueur courant et jouer le tour de l'ordinateur si nécessaire
       else {
-        joueurCourant = joueurCourant === 'saxo' ? 'skate' : 'saxo';
-        if (modeJeu === 'ordinateur' && joueurCourant === 'skate') {
+        joueurCourant = joueurCourant === "saxo" ? "skate" : "saxo";
+        if (modeJeu === "ordinateur" && joueurCourant === "skate") {
           jouerTourOrdinateur();
-        } else if (modeJeu === 'ordinateurPro' && joueurCourant === 'skate') {
+        } else if (modeJeu === "ordinateurPro" && joueurCourant === "skate") {
           jouerTourOrdinateurPro();
         }
       }
@@ -116,44 +116,62 @@ for (let i = 0; i < cases.length; i++) {
 
 // Fonction pour vérifier si un joueur a gagné
 function verifierGagnant() {
-  const lignes = [[0, 1, 2], [3, 4, 5], [6, 7, 8]];
-  const colonnes = [[0, 3, 6], [1, 4, 7], [2, 5, 8]];
-  const diagonales = [[0, 4, 8], [2, 4, 6]];
+  const lignes = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+  ];
+  const colonnes = [
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+  ];
+  const diagonales = [
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
 
   // Fonction interne pour vérifier une ligne gagnante
   function ligneGagnante(ligne) {
     const symbole = etatJeu[ligne[0]];
-    return symbole !== '' && ligne.every((indiceCase) => etatJeu[indiceCase] === symbole);
+    return (
+      symbole !== "" &&
+      ligne.every((indiceCase) => etatJeu[indiceCase] === symbole)
+    );
   }
 
   // Vérification des lignes, colonnes et diagonales pour déterminer s'il y a un gagnant
-  return lignes.some(ligneGagnante) || colonnes.some(ligneGagnante) || diagonales.some(ligneGagnante);
+  return (
+    lignes.some(ligneGagnante) ||
+    colonnes.some(ligneGagnante) ||
+    diagonales.some(ligneGagnante)
+  );
 }
 
 // Fonction pour afficher l'image de la victoire d'un joueur
 function afficherImageGagnante(joueur) {
-  let img = document.createElement('img');
-  img.src = joueur === 'saxo' ? 'saxowin.jpg' : 'skatewin.jpg';
-  img.alt = joueur + ' a gagné !';
-  img.classList.add('imagecentre');
-  resultatContainer.innerHTML = '';
+  let img = document.createElement("img");
+  img.src = joueur === "saxo" ? "saxowin.jpg" : "skatewin.jpg";
+  img.alt = joueur + " a gagné !";
+  img.classList.add("imagecentre");
+  resultatContainer.innerHTML = "";
   resultatContainer.appendChild(img);
 }
 
 // Fonction pour afficher l'image du match nul
 function afficherImageMatchNul() {
-  let img = document.createElement('img');
-  img.src = 'matchnul.jpg';
-  img.alt = 'Match nul !';
-  img.classList.add('imagecentre');
-  resultatContainer.innerHTML = '';
+  let img = document.createElement("img");
+  img.src = "matchnul.jpg";
+  img.alt = "Match nul !";
+  img.classList.add("imagecentre");
+  resultatContainer.innerHTML = "";
   resultatContainer.appendChild(img);
 }
 
 // Fonction pour masquer le plateau et les résultats
 function masquerPlateauEtResultat() {
-  plateau.style.display = 'none';
-  resultatContainer.style.display = 'block';
+  plateau.style.display = "none";
+  resultatContainer.style.display = "block";
 }
 
 // Fonction pour jouer le tour de l'ordinateur
@@ -161,7 +179,7 @@ function jouerTourOrdinateur() {
   // Recherche des cases vides
   let casesVides = [];
   for (let i = 0; i < etatJeu.length; i++) {
-    if (etatJeu[i] === '') {
+    if (etatJeu[i] === "") {
       casesVides.push(i);
     }
   }
@@ -176,7 +194,7 @@ function jouerTourOrdinateur() {
 // Fonction pour jouer le tour de l'ordinateur pro
 function jouerTourOrdinateurPro() {
   // Si c'est le premier tour, jouer au centre
-  if (etatJeu.filter(e => e !== '').length === 0) {
+  if (etatJeu.filter((e) => e !== "").length === 0) {
     jouerCase(4);
     return;
   }
@@ -209,7 +227,7 @@ function jouerTourOrdinateurPro() {
 // Fonction pour trouver un coup gagnant
 function trouverCoupGagnant() {
   for (let i = 0; i < etatJeu.length; i++) {
-    if (etatJeu[i] === '' && estCoupGagnant(i, joueurCourant)) {
+    if (etatJeu[i] === "" && estCoupGagnant(i, joueurCourant)) {
       return i;
     }
   }
@@ -228,7 +246,10 @@ function estCoupGagnant(index, joueur) {
 // Fonction pour trouver un coup bloquant (empêcher l'adversaire de gagner)
 function trouverCoupBloquant() {
   for (let i = 0; i < etatJeu.length; i++) {
-    if (etatJeu[i] === '' && estCoupGagnant(i, joueurCourant === 'saxo' ? 'skate' : 'saxo')) {
+    if (
+      etatJeu[i] === "" &&
+      estCoupGagnant(i, joueurCourant === "saxo" ? "skate" : "saxo")
+    ) {
       return i;
     }
   }
@@ -239,7 +260,7 @@ function trouverCoupBloquant() {
 function trouverCoinDisponible() {
   let coins = [4, 0, 2, 6, 8];
   for (let i = 0; i < coins.length; i++) {
-    if (etatJeu[coins[i]] === '') {
+    if (etatJeu[coins[i]] === "") {
       return coins[i];
     }
   }
@@ -255,7 +276,7 @@ function jouerCase(index) {
 function caseAleatoire() {
   let casesVides = [];
   for (let i = 0; i < etatJeu.length; i++) {
-    if (etatJeu[i] === '') {
+    if (etatJeu[i] === "") {
       casesVides.push(i);
     }
   }
